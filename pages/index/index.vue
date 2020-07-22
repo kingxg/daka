e8c642e022e37fd12158cf190ce6bd868da54398
superyan <hxgele@163.com>
2020-07-23 04:07:47 +0800

    index-activity-cover

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,10 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="page-wrap">
+		<view class="activity">
+			<view class="activity__cover">
+				<image class="cover-pic" src="../../static/index/cover.jpg" mode="aspectFill"></image>
+			</view>
 		</view>
-		<view>test</view>
 	</view>
 </template>
 
@@ -25,29 +25,16 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	.page-wrap {
+		background-color: #ededed;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	
+	.activity__cover {
+		height: 400rpx;
 	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	
+	.cover-pic {
+		width: 100%;
+		height: 100%;
 	}
 </style>
